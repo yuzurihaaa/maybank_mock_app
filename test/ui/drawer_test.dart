a35c39ba176471d26964a66dfab35ba4ad83def1
yuzuriha <zaralockheart@gmail.com>
2019-10-29 23:37:14 +0800

    test codecov config file

--- test/ui/drawer_test.dart
+++ test/ui/drawer_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:image_test_utils/image_test_utils.dart';
 import 'package:maybank2u/application/injector.dart';
-import 'package:maybank2u/main.dart';
+import 'package:maybank2u/application/application.dart';
 import 'package:maybank2u/ui/intro/widgets/drawer/drawer.dart';
 
 void main() {
