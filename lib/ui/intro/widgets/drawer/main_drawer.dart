0c9894909c25201f96ff413876286b477682ffb8
yuzuriha <zaralockheart@gmail.com>
2019-10-30 00:49:12 +0800

    added image on drawer.

--- lib/ui/intro/widgets/drawer/main_drawer.dart
+++ lib/ui/intro/widgets/drawer/main_drawer.dart
@@ -45,74 +45,95 @@ class MainDrawer extends HookWidget {
     }
 
     return Drawer(
-      child: Container(
-        color: Colors.black,
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  IconButton(
-                    key: Key('close button'),
-                    onPressed: () {
-                      if (state.value == DrawerState.Initial) {
-                        closeDrawer();
-                        animateIcon.forward();
-                      } else {
-                        _reverseView(DrawerState.Initial);
-                      }
-                    },
-                    icon: AnimatedIcon(
-                      icon: AnimatedIcons.menu_arrow,
-                      progress: animateIcon,
-                    ),
-                  )
-                ],
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 50),
-                child: Center(
-                  child: AnimatedSwitcher(
-                    duration: Duration(seconds: 1),
-                    child: Text(
-                      title,
-                      style: Theme.of(context).textTheme.headline,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: AnimatedSwitcher(
-                  duration: Duration(milliseconds: 400),
-                  child: (state.value == DrawerState.Initial ||
-                          state.value == DrawerState.Profile)
-                      ? Initial(
-                          key: Key('initial drawer'),
-                          state: state,
-                          onClickProfile: () =>
-                              state.value == DrawerState.Profile
-                                  ? _reverseView(DrawerState.Initial)
-                                  : _changeView(DrawerState.Profile),
-                        )
-                      : About(),
-                ),
-              ),
-              if (state.value == DrawerState.Initial)
-                FlatButton(
-                  key: Key('about button'),
-                  padding: EdgeInsets.all(0),
-                  onPressed: () {},
-                  child: Item(
-                    onPress: () => _changeView(DrawerState.About),
-                    first: true,
-                    icon: Icons.business,
-                    text: locale.about,
-                  ),
-                )
-            ],
+      child: Stack(
+        children: <Widget>[
+          ColorFiltered(
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(.5), BlendMode.srcATop),
+            child: Image.network(
+              'https://images4.alphacoders.com/117/thumb-350-11718.jpg',
+              height: 200,
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
+          ShaderMask(
+            shaderCallback: (rect) => LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment(0.0, -.3),
+              colors: [Colors.transparent, Colors.black, Colors.black],
+            ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height)),
+            blendMode: BlendMode.dstIn,
+            child: Container(
+              color: Colors.black,
+              child: SafeArea(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        IconButton(
+                          key: Key('close button'),
+                          onPressed: () {
+                            if (state.value == DrawerState.Initial) {
+                              closeDrawer();
+                              animateIcon.forward();
+                            } else {
+                              _reverseView(DrawerState.Initial);
+                            }
+                          },
+                          icon: AnimatedIcon(
+                            icon: AnimatedIcons.menu_arrow,
+                            progress: animateIcon,
+                          ),
+                        )
+                      ],
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 50),
+                      child: Center(
+                        child: AnimatedSwitcher(
+                          duration: Duration(seconds: 1),
+                          child: Text(
+                            title,
+                            style: Theme.of(context).textTheme.headline,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: AnimatedSwitcher(
+                        duration: Duration(milliseconds: 400),
+                        child: (state.value == DrawerState.Initial ||
+                                state.value == DrawerState.Profile)
+                            ? Initial(
+                                key: Key('initial drawer'),
+                                state: state,
+                                onClickProfile: () =>
+                                    state.value == DrawerState.Profile
+                                        ? _reverseView(DrawerState.Initial)
+                                        : _changeView(DrawerState.Profile),
+                              )
+                            : About(),
+                      ),
+                    ),
+                    if (state.value == DrawerState.Initial)
+                      FlatButton(
+                        key: Key('about button'),
+                        padding: EdgeInsets.all(0),
+                        onPressed: () {},
+                        child: Item(
+                          onPress: () => _changeView(DrawerState.About),
+                          first: true,
+                          icon: Icons.business,
+                          text: locale.about,
+                        ),
+                      )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
