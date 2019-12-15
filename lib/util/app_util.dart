a13ab65fc7f24fd4c4815abbe2a4e0505a4060b8
yuzuriha <zaralockheart@gmail.com>
2019-12-15 20:41:21 +0800

    upgrade to use flutter  1.12, migrate android x and set timer to always repeat when zero

--- lib/util/app_util.dart
+++ lib/util/app_util.dart
@@ -1,7 +1,13 @@
+import 'package:flutter/widgets.dart';
+
 String convertDigitToDoubleDigit(num input) {
   if (input < 10) {
     return '0$input';
   }
 
   return '$input';
+}
+
+void postFrameCallback(Function callback) {
+  WidgetsBinding.instance.addPostFrameCallback((_) => callback());
 }
